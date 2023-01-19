f632a96c68c0b34fd6f232e367b4f9496be31ba4
ShivumGupta <shivumgupta@googlemail.com>
2023-01-19 01:44:26 +0000

    Created a Quadratic Bezier Function

--- Bezier_Curves.py
+++ Bezier_Curves.py
@@ -3,23 +3,37 @@ import matplotlib.pyplot as plt
 
 def lerp(p1,p2,n):
     interpolated_points = np.ndarray((n+1,2))
+    steps = 1/n
     
-    xseparation = (p1[0] + p2[0]) / n
-    yseparation = (p1[1] + p2[1]) / n
-
-    initialx = p1[0]
-    initialy = p1[1]
     for i in range (n+1):
-        newx = initialx+(i*xseparation)
-        newy = initialy+(i*yseparation)
-        interpolated_points[i] = [newx,newy]
-    
+        t = i*steps
+        interpolated_points[i] = np.multiply((1-t),p1) + np.multiply(t,p2)
+
+    return interpolated_points
+
+def quadratic_Bezier(p1,p2,p3,n):
+    interpolated_points = np.ndarray((n+1,2))
+    lerp1 = lerp (p1,p2,n)
+    lerp2 = lerp (p2,p3,n)
+
+    for i in range (n+1):
+        lerp_of_lerps = lerp(lerp1[i],lerp2[i],n)[i]
+        interpolated_points[i] = [lerp_of_lerps[0],lerp_of_lerps[1]]
+
     return interpolated_points
 
 p1 = [0,0]
 p2 = [10,10]
-n = 5
+p3 = [15,0]
+n = 50
 
 points = lerp(p1,p2,n)
-plt.plot(points[:,0],points[:,1])
+print (points)
+points2 = lerp(p2,p3,n)
+qpoints = quadratic_Bezier(p1,p2,p3,n)
+plt.plot(points[:,0],points[:,1],"o-")
+plt.plot(points2[:,0],points2[:,1],"o-")
+plt.plot(qpoints[:,0],qpoints[:,1],"-")
 plt.show()
+
+#print (qpoints)
