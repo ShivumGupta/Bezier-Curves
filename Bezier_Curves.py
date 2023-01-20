efacab5a99f7da3fe53ee20b08746f90412cd436
ShivumGupta <shivumgupta@googlemail.com>
2023-01-20 01:00:43 +0000

    Basic Lerp Animation

--- Bezier_Curves.py
+++ Bezier_Curves.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 def lerp(p1,p2,n):
     interpolated_points = np.ndarray((n+1,2))
@@ -34,23 +35,37 @@ def cubic_Bezier(p1,p2,p3,p4,n):
     return interpolated_points
 
 
-p1 = [0,0]
-p2 = [-100,170]
+p1 = [10,10]
+p2 = [100,170]
 p3 = [150,920]
 p4 = [200,7]
 n = 50
 
-qpoints = quadratic_Bezier(p1,p2,p3,n)
-plt.plot(qpoints[:,0],qpoints[:,1],"-")
+# qpoints = quadratic_Bezier(p1,p2,p3,n)
+# plt.plot(qpoints[:,0],qpoints[:,1],"-")
 
-cpoints = cubic_Bezier(p1,p2,p3,p4,n)
-plt.plot(cpoints[:,0],cpoints[:,1],"-")
+# cpoints = cubic_Bezier(p1,p2,p3,p4,n)
+# plt.plot(cpoints[:,0],cpoints[:,1],"-")
 
-cols = ["tab:blue","tab:orange","tab:red","tab:green","tab:pink"]
-# for i in range (1,6):
-#     qpoints = quadratic_Bezier(p1,np.multiply(p2,i),p3,n)
-#     plt.plot(qpoints[:,0],qpoints[:,1],"-",color=cols[i-1])
-#     plt.scatter(np.multiply(p2,i)[0],np.multiply(p2,i)[1],color=cols[i-1])
+# cols = ["tab:blue","tab:orange","tab:red","tab:green","tab:pink"]
 
+lerp_points = lerp(p1,p2,n)
+draw = lerp_points
 
+fig = plt.figure()
+ax = plt.axes(xlim=(0,1.1*max(draw[:,0])), ylim=(0,1.1*max(draw[:,1])))
+line, = ax.plot([],[],"-")
+ax.scatter([p1[0],p2[0]],[p1[1],p2[1]],color="gray")
+points = ax.scatter(p1[0],p1[1],color="black")
+
+def update(i):
+    to_draw = draw[:i]
+    xpoints = to_draw[:,0]
+    ypoints = to_draw[:,1]
+    line.set_xdata (xpoints)
+    line.set_ydata (ypoints)
+    points.set_offsets([to_draw[-1][0],to_draw[-1][1]])
+    return line,
+
+anim = animation.FuncAnimation(fig, update, interval=100,blit=False,frames=np.arange(1,n+2,1),repeat=False)
 plt.show()
